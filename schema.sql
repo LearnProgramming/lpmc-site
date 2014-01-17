eeaf5a871a6057989b344bfda586b52f9f8fcb2f
jmw327 <josh@jmw327.com>
2014-01-16 23:51:21 -0600

    add basic schema

---
+++ schema.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+        id serial PRIMARY KEY,
+		github_id integer NOT NULL UNIQUE,
+        username varchar(32) NOT NULL UNIQUE,
+		access_token varchar NOT NULL UNIQUE
+);
