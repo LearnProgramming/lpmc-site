c9f3f65cb4dc0e20a203cc704aaf8f63f4fa77c7
jmw327 <josh@jmw327.com>
2014-03-28 17:11:00 -0700

    better email selection and new stylez

--- static/js/account.js
+++ static/js/account.js
@@ -1,21 +1,35 @@
 window.addEvent('domready', function() {
 	'use strict';
 
-	$('update_emails').addEvent('click', function() {
-		new Request.JSON({
-			'url': '/github_emails',
-			'onSuccess': function(response) {
-				var emails = $('emails');
-				response.each(function(email) {
-					var div = new Element('div');
-					div.appendText(email['email']);
-					div.addEvent('click', function() {
-						set_email(email['email']);
+	document.addEvent('click', function() {
+		var emails = $('emails');
+		emails.set('html', null);
+		emails.setProperty('data-open', 'false');
+	});
+
+	$('update-emails').addEvent('click', function() {
+		var emails = $('emails');
+		var open = emails.getProperty('data-open');
+		if (open !== 'true') {
+			new Request.JSON({
+				'url': '/github_emails',
+				'onSuccess': function(response) {
+					emails.setProperty('data-open', 'true');
+					emails.set('html', null);
+					response.each(function(email) {
+						var div = new Element('div');
+						div.appendText(email['email']);
+						div.addEvent('click', function() {
+							set_email(email['email']);
+						});
+						emails.grab(div, 'top');
 					});
-					emails.grab(div, 'top');
-				});
-			},
-		}).get();
+				},
+			}).get();
+		} else {
+			emails.set('html', null);
+			emails.setProperty('data-open', 'false');
+		}
 	});
 
 	function set_email(email) {
