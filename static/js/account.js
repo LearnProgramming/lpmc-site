1e0ce2bda247505d825841daba928f6fe0ee0dcf
Lance Batson <lancebatsondev@gmail.com>
2014-04-03 08:14:41 -0500

    working on account page

--- static/js/account.js
+++ static/js/account.js
@@ -1,44 +1,50 @@
-window.addEvent('domready', function() {
+// window.addEvent('domready', function() {
+// 	'use strict';
+//
+// 	document.addEvent('click', function() {
+// 		var emails = $('emails');
+// 		emails.set('html', null);
+// 		emails.setProperty('data-open', 'false');
+// 	});
+//
+// 	$('update-emails').addEvent('click', function() {
+// 		var emails = $('emails');
+// 		var open = emails.getProperty('data-open');
+// 		if (open !== 'true') {
+// 			new Request.JSON({
+// 				'url': '/github_emails',
+// 				'onSuccess': function(response) {
+// 					emails.setProperty('data-open', 'true');
+// 					emails.set('html', null);
+// 					response.each(function(email) {
+// 						var div = new Element('div');
+// 						div.appendText(email['email']);
+// 						div.addEvent('click', function() {
+// 							set_email(email['email']);
+// 						});
+// 						emails.grab(div, 'top');
+// 					});
+// 				},
+// 			}).get();
+// 		} else {
+// 			emails.set('html', null);
+// 			emails.setProperty('data-open', 'false');
+// 		}
+// 	});
+//
+// 	function set_email(email) {
+// 		new Request({
+// 			'url': '/account/contact_info',
+// 			'onSuccess': function(response) {
+// 				if (response)
+// 					location.reload();
+// 			},
+// 		}).post({'info_type': 0, 'info': email});
+// 	}
+// });
+
+$(function() {
 	'use strict';
 
-	document.addEvent('click', function() {
-		var emails = $('emails');
-		emails.set('html', null);
-		emails.setProperty('data-open', 'false');
-	});
-
-	$('update-emails').addEvent('click', function() {
-		var emails = $('emails');
-		var open = emails.getProperty('data-open');
-		if (open !== 'true') {
-			new Request.JSON({
-				'url': '/github_emails',
-				'onSuccess': function(response) {
-					emails.setProperty('data-open', 'true');
-					emails.set('html', null);
-					response.each(function(email) {
-						var div = new Element('div');
-						div.appendText(email['email']);
-						div.addEvent('click', function() {
-							set_email(email['email']);
-						});
-						emails.grab(div, 'top');
-					});
-				},
-			}).get();
-		} else {
-			emails.set('html', null);
-			emails.setProperty('data-open', 'false');
-		}
-	});
-
-	function set_email(email) {
-		new Request({
-			'url': '/account/contact_info',
-			'onSuccess': function(response) {
-				if (response)
-					location.reload();
-			},
-		}).post({'info_type': 0, 'info': email});
-	}
+	console.log('test');
 });
